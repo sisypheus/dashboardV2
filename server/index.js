d9a0a6bfb3b56d390d8c418068ef0688804f1213
Theo <theo.poette@epitech.eu>
2021-11-11 18:07:11 +0100

    feat: server about.json and docker compose for server

--- server/index.js
+++ server/index.js
@@ -8,21 +8,37 @@ import weatherRoutes from './routes/services/weather.js';
 dotenv.config();
 app.use(cors());
 
+app.set('trust proxy', true);
+
 app.use('/service/weather', weatherRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is up and running')
 })
 
 app.get('/about.json', (req, res) => {
+  console.log(req.ip.toString());
   res.json({
-    name: 'John Doe',
-    age: 30
+    client: {
+      host: req.ip
+    },
+    server: {
+      current_time: (new Date().getTime() / 1000).toString().split('.')[0],
+      services: [{
+        name: "weather",
+        widgets: "city weather",
+        description: "get the temperature and brief description of current weather in given city",
+        params: [{
+          name: "city",
+          type: "string"
+        }]
+      }]
+    }
   })
 });
 
 app.get
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(`Example app listening`);
 })
