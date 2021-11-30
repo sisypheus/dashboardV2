d0110d7953cfb2f063c3fdfc626ad7fd77ea3d5f
Alizée <alizee.soussan@epitech.eu>
2021-11-30 19:06:31 +0100

    feat: define theme colors in tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,11 +4,24 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'dark-sidebar': '#0d1117',
-        'dark-nav': '#161b22',
-        'dark': '#010409'
+        'dark-sidebar': '#22272e',
+        'dark-nav': '#2d333b',
+        'dark': '#1c2128',
+        'light-sidebar': '#c5c8c8',
+        'light-nav': '#18415d',
+        'light': '#dbdbdb',
+        'widgets': '#2d333b'
       },
-      textColor: ['checked'],
+      textColor: {
+        'orange': '#faae5e',
+        'green': '#50a38a',
+        'purple': '#7e8ce0',
+        'blue': '#48c7d0',
+        'saumon': '#f7a38e',
+        'red' :'#df4576',
+        'text': '#506477',
+        'sidebar': '#687078',
+      },
       color: ['checked'],
     }
   },
