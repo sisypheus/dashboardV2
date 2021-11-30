54d0a1d527f2728644254c0b146f544e0b5fc0cb
Alizée <alizee.soussan@epitech.eu>
2021-11-30 19:13:22 +0100

    fix: changed color of background to match theme

--- client/src/utils/Background.js
+++ client/src/utils/Background.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Background = ({children}) => {
   return (
-    <div className="min-h-screen bg-white dark:bg-dark transition-all">
+    <div className="min-h-screen bg-light dark:bg-dark transition-all">
       {children}
     </div>
   );
